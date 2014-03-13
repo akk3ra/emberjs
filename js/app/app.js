fb69afadd486a5f8e73b4643fd976589710119be
Sandeep Akkeera <sandeepakkeera@Sandeeps-MacBook-Air.local>
2014-03-13 01:16:03 -0500

    Made changes to the files..

--- js/app/app.js
+++ js/app/app.js
@@ -10,9 +10,24 @@ Cricket.Router.map(function(){
 	this.route('index', {path: '/'});
 	this.route('login', {path: '/login'});
 	this.route('register', {path: '/register'});
-	this.route('welcome', {path: '/welcome'});
+	this.resource('home', {path: '/home'}, function(){
+		this.route('index',{path: '/welcome'});
+		this.route('teams', {path: '/teams'});
+		this.route('signoff', {path: '/signoff'});
+		this.route('guest', {path: '/guest'});
+	});
+	this.route('loginError', {path: '/loginError'});
 });
-
+Cricket.HomeRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.find('homeData');
+	}
+});
+Cricket.HomeTeamsRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.find('homeData');
+	}
+});
 Cricket.IndexRoute = Ember.Route.extend({
 	redirect: function(){
 		this.transitionTo('login');
@@ -29,16 +44,13 @@ Cricket.RegisterRoute = Ember.Route.extend({
 		console.log('Loaded the register page..');
 	}
 });
-Cricket.WelcomeRoute = Ember.Route.extend({
+Cricket.LoginErrorRoute = Ember.Route.extend({
 	model: function(){
-		console.log('Loaded the welcome page..');
+		console.log("Login error page loaded as the credentials were wrong!!!!");
 	}
 });
-Cricket.RegisterController = Ember.ObjectController.extend({
 
-	actions: {}
-});
-Cricket.WelcomeController = Ember.ObjectController.extend({
+Cricket.RegisterController = Ember.ObjectController.extend({
 
 	actions: {}
 });
@@ -59,6 +71,7 @@ Cricket.LoginController = Ember.ObjectController.extend({
 		//Get hold of the current controllers context to use later
 		var currController = this;
 		var content = this.get('content');
+		var userPresent = false;
 		content.forEach(function(loginCred){
 			console.log("userid from DB...."+loginCred.id);
 			console.log("userid from DB...."+loginCred.get('userId'));
@@ -66,11 +79,14 @@ Cricket.LoginController = Ember.ObjectController.extend({
 
 			if(loginCred.id==userId && loginCred.get('password')==pwd){
 				console.log("Authentication successful!!!!!!!");
-				currController.transitionToRoute('welcome');
-			}else{
-				console.log("Authentication failed !!!!!!, Please enter correct credentials..");
+				userPresent = true;
+				currController.transitionToRoute('home');
 			}
 		});
+		if(!userPresent){
+			console.log("Authentication failed !!!!!!, Please enter correct credentials..");
+			currController.transitionToRoute('loginError');
+		}
 	}
 	}
 	
@@ -78,74 +94,31 @@ Cricket.LoginController = Ember.ObjectController.extend({
 Cricket.Store = DS.Store.extend({
 	adapter: DS.FixtureAdapter
 });
+Cricket.HomeData = DS.Model.extend({
+	teamName: DS.attr('string')
+});
+Cricket.HomeData.FIXTURES = [{id: 'mightyMinns',
+							teamName: 'Mighty Minns'},
+							{id: 'hydHawks',
+							teamName: 'Hyd Hawks'},
+							{id: 'royalTigers',
+							teamName: 'Royal Tigers'},
+							{id: 'sunRisers',
+							teamName: 'Sunrisers'}
+							];
 Cricket.CricketLogin = DS.Model.extend({
 	userId: DS.attr('string'),
 	password: DS.attr('string')
 });
-Cricket.CricketLogin.FIXTURES = [{id: 'jerrgof@gmail.com',
+Cricket.CricketLogin.FIXTURES = [{id: 'a',
+								 userId: 'a', 
+								 password: 'a'
+								},
+								{id: 'jerrgof@gmail.com',
 								 userId: 'jerrgof@gmail.com', 
 								 password: 'Password1'
 								},
 								{id: 'sandeep.aks@gmail.com',
 								 userId: 'sandeep.aks@gmail.com', 
 								 password: 'Password1'
-								}]
-Cricket.CricketData = DS.Model.extend({
-
-	postTitle: DS.attr('string'),
-	postName: DS.attr('string'),
-	postLongIntro: DS.attr('string'),
-	postShortIntro: DS.attr('string'),
-	postDate: DS.attr('date'),
-	formattedDate: function(){
-		var date = this.get('postDate');
-		if(date){
-			return this.get('postDate').getUTCDay() 
-				+ "/" + (this.get('postDate').getUTCMonth() + 1) 
-					+ "/" + this.get('postDate').getUTCFullYear();
-
-		}
-		else{	
-			return '';
-		}
-	}.property('postDate')
-
-});
-
-Cricket.CricketData.FIXTURES = [
-	{
-	id:1, postTitle: 'Explaining Object equality in scala', 
-	postDate: new Date(),
-	postName: 'Scala Blog', postShortIntro:'This is Object Equality', 
-	postLongIntro:'This is Object Equality', 
-	postFileName: 'scala.blog'
-	},
-	{
-	id:2, postTitle: 'Explaining Implicit conversion in scala', 
-	postDate: new Date(),
-	postName: 'Scala Blog', postShortIntro:'This is Object Equality', 
-	postLongIntro:'This is Implicit Conversion', 
-	postFileName: 'scala.blog'
-	},
-	{
-	id:3, postTitle: 'Explaining FUnctional Literal in scala', 
-	postDate: new Date(),
-	postName: 'Scala Blog', postShortIntro:'This is Object Equality', 
-	postLongIntro:'This is Function literal', 
-	postFileName: 'scala.blog'
-	},
-	{
-	id:4, postTitle: 'Explaining OOP in scala', 
-	postDate: new Date(),
-	postName: 'Scala Blog', postShortIntro:'This is Object Equality', 
-	postLongIntro:'This is OOP in Scala', 
-	postFileName: 'scala.blog'
-	},
-	{
-	id:5, postTitle: 'Explaining Actors in scala',
-	postDate: new Date(), 
-	postName: 'Scala Blog', postShortIntro:'This is Object Equality', 
-	postLongIntro:'This is Actors In Scala', 
-	postFileName: 'Scala.blog'
-	}
-];
+								}];
