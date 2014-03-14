466cf55042e70fc9f096444575a65ccc7ea46798
Sandeep Akkeera <sandeepakkeera@Sandeeps-MacBook-Air.local>
2014-03-14 01:25:50 -0500

    Removed some unnecessary files...

--- js/app/home/teams/team/team_fixture.js
+++
@@ -1,9 +0,0 @@
-Cricket.Team = DS.Model.extend({
-	teamName: DS.attr('string'),
-	players: DS.hasMany('player'),
-	from: DS.attr('string')
-});
-Cricket.Team.FIXTURES = [{},
-						{},
-						{},
-						{}];
